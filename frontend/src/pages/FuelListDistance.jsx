aa19fd2cf736d48ea4d3884cf0d1a51184aaa31b
krish <krishrajani680@gmail.com>
2025-07-28 14:56:58 -0600

    updated distance page to show the savings using utils/savings.js

--- frontend/src/pages/FuelListDistance.jsx
+++ frontend/src/pages/FuelListDistance.jsx
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { fetchDistanceOnly } from '../api/distanceOnly';
+import {getNearestStation, calculateCentDifference} from '../utils/savings';
 import '../styles/FuelList.css';
 
 const FuelListDistance = ({ userLocation }) => {
   const [stations, setStations] = useState([]);
 
+
   useEffect(() => {
     if (userLocation) {
       fetchDistanceOnly(userLocation)
         .then((data) => {
-          const sorted = data
-            .filter(station => station.distance !== null)
-            .sort((a, b) => a.distance - b.distance);
-          setStations(sorted);
+          const filtered = data.filter(station => station.distance !== null);
+          const sorted = [...filtered].sort((a, b) => a.distance - b.distance);
+          const nearest = getNearestStation(filtered);
+
+          const updated = sorted.map((station) => {
+            const centsSaved = calculateCentDifference(nearest.price, station.price);
+            return { ...station, centsSaved };
+          });
+
+          setStations(updated);
         })
         .catch((err) => {
           console.error("Failed to fetch distances:", err);
         });
     }
   }, [userLocation]);
+
 
   const handleGetDirections = (lat, lng) => {
     const origin = `${userLocation.lat},${userLocation.lng}`;
@@ -36,6 +45,9 @@ const FuelListDistance = ({ userLocation }) => {
           <li key={index} className="station-card">
             <strong>{station.station_name}</strong>
             <div className="station-meta">{station.address} - ${station.price.toFixed(2)}</div>
+            {station.centsSaved && (
+              <div className='station-meta savings-text'>{station.centsSaved}</div>
+            )}
             <div className="station-meta">
               Distance: {station.distance_text} ({station.duration_text})
             </div>
