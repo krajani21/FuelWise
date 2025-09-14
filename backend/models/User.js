acfb29394ab597b58cbfd0145df23aed482130d8
krish <krishrajani680@gmail.com>
2025-09-14 15:37:28 -0600

    changed User.js back to test an issue

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,8 +5,6 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  resetToken: { type: String },
-  resetTokenExpiry: { type: Date },
 }, { timestamps: true });
 
 // Hash password before saving
