674d11243af464bc3d0f93636b778aa4b5239d6c
krish <krishrajani680@gmail.com>
2025-09-22 12:14:27 -0600

    added vercel analytics, need to test to see if it works

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { inject } from '@vercel/analytics';   // 👈 import inject
+
+inject();  // 👈 call it once, before rendering
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
