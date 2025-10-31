62f252da14127ddfa5f3f804ffce7648a5f0da6e
krish <krishrajani680@gmail.com>
2025-10-31 16:57:46 -0600

    added staging URL in CORS and added correct URL in api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,5 @@
 // API Configuration
 //export const API_BASE_URL = 'https://fuelwise-1der.onrender.com/api'; // this is the production URL
-export const API_BASE_URL = 'https://fuel-wise-git-staging-krishs-projects-17bcb96e.vercel.app/'; // this is the staging URL
+export const API_BASE_URL = 'https://fuelwise-api-staging.onrender.com/api'; // this is the staging URL
 
 
