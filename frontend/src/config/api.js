1805b474f7169d5b765ef35bb9e62751eb614775
krish <krishrajani680@gmail.com>
2025-10-31 16:23:22 -0600

    changed API_BASE_URL to just be in the same file and not in the .env

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,5 @@
 // API Configuration
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+//export const API_BASE_URL = 'https://fuelwise-1der.onrender.com/api'; // this is the production URL
+export const API_BASE_URL = 'https://fuel-wise-git-staging-krishs-projects-17bcb96e.vercel.app/api'; // this is the staging URL
+
+
